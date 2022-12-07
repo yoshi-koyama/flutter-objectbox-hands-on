7293b473c75fc1e4bdb5303a24ac3fa8a8673d67
小山由人 <koyama@reytech.co.jp>
2022-12-07 23:15:37 +0900

    ハンズオン実施

--- lib/view/ToDoPage.dart
+++ lib/view/ToDoPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_objectbox_hands_on/objectbox.g.dart';
 
+import '../model/ToDo.dart';
 import 'ShoppingMemoPage.dart';
 
 class ToDoPage extends StatefulWidget {
@@ -13,6 +14,8 @@ class ToDoPage extends StatefulWidget {
 class _ToDoPageState extends State<ToDoPage> {
   // ObjectBoxの利用にstoreが必要
   Store? store;
+  Box<ToDo>? toDoBox;
+  List<ToDo> toDoList = [];
 
   // 投稿後の内容を削除するためのもの
   final controller = TextEditingController();
@@ -23,7 +26,8 @@ class _ToDoPageState extends State<ToDoPage> {
   Future<void> initialize() async {
     // storeの作成にopenStore()という非同期関数の実行が必要
     store = await openStore();
-    setState(() {});
+    toDoBox = store?.box<ToDo>();
+    fetchToDoList();
   }
 
   // この関数の中の処理は初回に一度だけ実行される
@@ -31,6 +35,12 @@ class _ToDoPageState extends State<ToDoPage> {
   void initState() {
     super.initState();
     initialize();
+  }
+
+  // BoxからToDo一覧を取得
+  void fetchToDoList() {
+    toDoList = toDoBox?.getAll() ?? [];
+    setState(() {});
   }
 
   // controllerを使う時はdisposeが必要
@@ -57,7 +67,10 @@ class _ToDoPageState extends State<ToDoPage> {
             icon: const Icon(Icons.shopping_cart, size: 28),
             onPressed: () {
               store?.close();
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const ShoppingMemoPage()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ShoppingMemoPage()));
             },
           )
         ],
@@ -66,24 +79,40 @@ class _ToDoPageState extends State<ToDoPage> {
         children: [
           Expanded(
             child: ListView.builder(
-              itemCount: sampleList.length,
+              itemCount: toDoList.length,
               itemBuilder: (context, index) {
-                final sample = sampleList[index];
+                final toDo = toDoList[index];
                 return Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Row(
                     children: [
+                      // チェックボックスの表示
+                      Transform.scale(
+                        scale: 1.3,
+                        child: Checkbox(
+                          activeColor: Colors.blue,
+                          value: toDo.check,
+                          onChanged: (bool? checkBoxState) {
+                            toDo.check = checkBoxState!;
+                            toDoBox?.put(toDo);
+                            fetchToDoList();
+                          },
+                        ),
+                      ),
                       // ToDoリストの内容を表示
                       Expanded(
                         child: Text(
-                          sample,
-                          style: const TextStyle(fontSize: 28),
+                          toDo.todo,
+                          style: TextStyle(
+                              fontSize: 28,
+                              color: toDo.check ? Colors.grey : Colors.black),
                         ),
                       ),
                       // ToDoリストの削除
                       IconButton(
                         onPressed: () {
-                          // ToDo ToDoリストの削除処理を実装
+                          toDoBox?.remove(toDo.id);
+                          fetchToDoList();
                         },
                         icon: const Icon(Icons.delete, size: 28),
                       )
@@ -115,7 +144,11 @@ class _ToDoPageState extends State<ToDoPage> {
                 ),
               ),
               onFieldSubmitted: (text) {
-                // ToDo ToDoリストの登録処理を実装
+                if (text != "") {
+                  final newToDo = ToDo(todo: text, check: false);
+                  toDoBox?.put(newToDo);
+                  fetchToDoList();
+                }
                 controller.clear();
               },
             ),
